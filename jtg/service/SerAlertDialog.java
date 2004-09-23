8876acd0baffc5b49ac2de32a80f9dc74daa179b
alexg <alexg>
2004-09-23 14:54:14 +0000

    +alertConnectionLost

--- jtg/service/SerAlertDialog.java
+++ jtg/service/SerAlertDialog.java
@@ -7,18 +7,29 @@ import java.awt.Component;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
+import org.apache.log4j.Logger;
+
 
 public class SerAlertDialog extends JFrame //implements ActionListener
 {
 	
-	public static void alert(String nachricht, Component comp ) 
-		{ 
-			JOptionPane.showMessageDialog(
-							comp,
-							nachricht,
-							"Achtung!",
-							JOptionPane.YES_OPTION
-							);
-		}	
+	public static void alert(String nachricht, Component comp )  { 
+		JOptionPane.showMessageDialog(
+			comp,
+			nachricht,
+			"Achtung!",
+			JOptionPane.YES_OPTION
+		);
+	}
+	
+	public static void alertConnectionLost( String loggingClass, Component comp )  { 
+		JOptionPane.showMessageDialog(
+			comp,
+			"No connection to box",
+			"Achtung!",
+			JOptionPane.YES_OPTION
+		);
+		Logger.getLogger(loggingClass).error("No connection to box");
+	}
 }
 
