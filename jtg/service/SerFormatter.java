a3a47977c6e44ef354113063fb2371ff65a2cacc
ralix <ralix>
2004-09-23 17:36:19 +0000

    %formatLong()

--- jtg/service/SerFormatter.java
+++ jtg/service/SerFormatter.java
@@ -45,13 +45,11 @@ public class SerFormatter {
 	} 
 		
 	private static long formatLong(String date){
-		long a = 0L;
 		return Long.parseLong(date) * 1000;                          
 	}
 	
-	public static String getUnixEndTime(String start, String dauer){
-    	Long l = new Long((formatLong(start) + formatLong(dauer)));  
-    	return l.toString();   	
+	public static String getUnixEndTime(String start, String dauer){ 	
+		return new Long((formatLong(start) + formatLong(dauer))).toString();
     }
                         
 	public static String formatUnixTime(String date, String dauer){
