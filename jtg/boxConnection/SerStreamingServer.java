ecbc7f0c8c88b15bc668384f9a51358804f020c7
alexg <alexg>
2004-10-10 21:34:02 +0000

    Implemented Parsing XML-Record-Document to BORecordArgs

--- jtg/boxConnection/SerStreamingServer.java
+++ jtg/boxConnection/SerStreamingServer.java
@@ -1,13 +1,20 @@
 package boxConnection;
 
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import model.BORecordArgs;
+
 import org.apache.log4j.Logger;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
+
+import service.SerXMLConverter;
 
 /**
  * @author Geist Alexander
@@ -16,6 +23,7 @@ import org.apache.log4j.Logger;
 public class SerStreamingServer extends Thread {
 	
 	int port = 4000;
+	ServerSocket server;
 	
 	public SerStreamingServer(int port) {
 		this.setPort(port);
@@ -23,20 +31,32 @@ public class SerStreamingServer extends Thread {
 	
 	public void run() {
 		try {
-			ServerSocket server = new ServerSocket(this.getPort());
+			server = new ServerSocket(this.getPort());
 			Socket socket = server.accept();
-			BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			
-			String line;
-			while((line=input.readLine())!=null) {
-				System.out.print(line);
-			}
-			server.close();  //server restart
-			this.run();
+			this.record(socket);
 		} catch (IOException e) {
-			Logger.getLogger("ControlMain").error("StreamingServer start failure");	
+			Logger.getLogger("ControlMain").error("StreamingServer start failed");	
+		} catch (DocumentException e) {
+			Logger.getLogger("ControlMain").error("Recording failed");	
 		}
 	}
+	
+	public void record(Socket socket) throws IOException, DocumentException {		
+		SAXReader reader = new SAXReader();
+		Document document = reader.read(socket.getInputStream());
+		this.write(document);
+		BORecordArgs recordArgs = SerXMLConverter.parseRecordDocument(document);
+		
+		server.close();  //server restart
+		this.run();
+	}
+	
+	public void write(Document document) throws IOException {	
+        OutputFormat format = OutputFormat.createPrettyPrint();
+        XMLWriter writer = new XMLWriter( System.out, format );
+        writer.write( document );
+    }
+	
 	/**
 	 * @return Returns the port.
 	 */
