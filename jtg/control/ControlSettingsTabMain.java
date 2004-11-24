68b736f8464ebcc07f0d5bde6ba2398fcd807c92
crazyreini <crazyreini>
2004-11-24 17:36:28 +0000

    %itemStateChanged:
    AUfruf der Methode checkTrayMenu um den Tray ein bzw. auszuschalten

--- jtg/control/ControlSettingsTabMain.java
+++ jtg/control/ControlSettingsTabMain.java
@@ -1,24 +1,17 @@
 /*
-ControlSettingsTabMain.java by Geist Alexander 
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2, or (at your option)
-any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  
-
-*/ 
+ * ControlSettingsTabMain.java by Geist Alexander
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ */
 package control;
-
-
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -34,27 +27,29 @@ import presentation.GuiMainView;
 import presentation.GuiSettingsTabMain;
 import presentation.GuiTabSettings;
 
-public class ControlSettingsTabMain extends ControlTabSettings implements ActionListener, ItemListener{
-    
-    GuiTabSettings settingsTab;
-    public final String[] localeNames = { "de,Deutsch", "en,Englisch","fi,Finisch"	};
-    
-    public ControlSettingsTabMain (GuiTabSettings tabSettings) {
+public class ControlSettingsTabMain extends ControlTabSettings implements ActionListener, ItemListener {
+
+	GuiTabSettings			settingsTab;
+	public final String[]	localeNames	= {"de,Deutsch", "en,Englisch", "fi,Finisch"};
+
+	public ControlSettingsTabMain(GuiTabSettings tabSettings) {
 		this.setSettingsTab(tabSettings);
 	}
-    
-    /* (non-Javadoc)
-     * @see control.ControlTab#initialize()
-     */
-    public void initialize() {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see control.ControlTab#initialize()
+	 */
+	public void initialize() {
 		this.getTab().getJComboBoxTheme().setSelectedItem(this.getSettings().getThemeLayout());
 		this.getTab().getJComboBoxLocale().setSelectedItem(this.getSettings().getLocale());
 		this.getTab().getCbShowLogo().setSelected(this.getSettings().isShowLogo());
 		this.getTab().getCbStartFullscreen().setSelected(this.getSettings().isStartFullscreen());
 		this.getTab().getCbUseSysTray().setSelected(this.getSettings().isUseSysTray());
-    }
-    
-    public void actionPerformed(ActionEvent e) {
+	}
+
+	public void actionPerformed(ActionEvent e) {
 		String action = e.getActionCommand();
 		if (action == "delete") {
 			this.actionRemoveBox();
@@ -63,45 +58,53 @@ public class ControlSettingsTabMain extends ControlTabSettings implements Action
 			this.actionAddBox();
 		}
 	}
-    
-//  Change-Events der Combos und der Checkbox
-	public void itemStateChanged (ItemEvent event) {
+
+	/**
+	 * Change-Events der Combos und der Checkbox
+	 * 
+	 * @version 24.11.2004 12:00
+	 */
+	public void itemStateChanged(ItemEvent event) {
 		String comp = event.getSource().getClass().getName();
 		while (true) {
 			if (comp.equals("javax.swing.JCheckBox")) {
-				JCheckBox checkBox = (JCheckBox)event.getSource();
+				JCheckBox checkBox = (JCheckBox) event.getSource();
 				if (checkBox.getName().equals("showLogo")) {
 					this.getSettings().setShowLogo(checkBox.isSelected());
 					break;
 				}
 				if (checkBox.getName().equals("startFullscreen")) {
-				    this.getSettings().setStartFullscreen(checkBox.isSelected());
+					this.getSettings().setStartFullscreen(checkBox.isSelected());
 					break;
 				}
 				if (checkBox.getName().equals("useSysTray")) {
-				    this.getSettings().setUseSysTray(checkBox.isSelected());
+					boolean useTray = checkBox.isSelected();
+					this.getSettings().setUseSysTray(useTray);
+
+					this.getMainView().checkTrayMenu(useTray); // Damit das Tray sofort geladen, bzw. ausgeblendet wird
+
 					break;
 				}
 				break;
-			} else {
-				JComboBox comboBox = (JComboBox)event.getSource();
-				if (event.getStateChange()==1) {
+			}
+			else {
+				JComboBox comboBox = (JComboBox) event.getSource();
+				if (event.getStateChange() == 1) {
 					if (comboBox.getName().equals("theme")) {
-					    getSettings().setThemeLayout((String)comboBox.getSelectedItem());
+						getSettings().setThemeLayout((String) comboBox.getSelectedItem());
 						break;
 					}
 					if (comboBox.getName().equals("locale")) {
-					    getSettings().setLocale((String)comboBox.getSelectedItem());
+						getSettings().setLocale((String) comboBox.getSelectedItem());
 						break;
 					}
-				}	
+				}
 				break;
-			}	
+			}
 		}
-		
 	}
-        
-    private void actionAddBox() {
+
+	private void actionAddBox() {
 		BOBox box = new BOBox();
 		this.getSettingsTab().getSettingsTabMain().getModelBoxTable().addRow(box);
 	}
@@ -110,27 +113,31 @@ public class ControlSettingsTabMain extends ControlTabSettings implements Action
 		this.getSettingsTab().getSettingsTabMain().getModelBoxTable().removeRow(selectedRow);
 	}
 
-    /* (non-Javadoc)
-     * @see control.ControlTab#getMainView()
-     */
-    public GuiTabSettings getSettingsTab() {
-        return settingsTab;
-    }
-    public GuiMainView getMainView() {
-        return this.getSettingsTab().getControl().getMainView();
-    }
-    /* (non-Javadoc)
-     * @see control.ControlTab#setMainView(presentation.GuiMainView)
-     */
-    public void setSettingsTab(GuiTabSettings tabSettings) {
-        settingsTab = tabSettings;
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see control.ControlTab#getMainView()
+	 */
+	public GuiTabSettings getSettingsTab() {
+		return settingsTab;
+	}
+	public GuiMainView getMainView() {
+		return this.getSettingsTab().getControl().getMainView();
+	}
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see control.ControlTab#setMainView(presentation.GuiMainView)
+	 */
+	public void setSettingsTab(GuiTabSettings tabSettings) {
+		settingsTab = tabSettings;
 
-    }
-    private BOSettings getSettings() {
-        return ControlMain.getSettings();
-    }
-    
-    private GuiSettingsTabMain getTab() {
-        return this.getSettingsTab().getSettingsTabMain();
-    }
+	}
+	private BOSettings getSettings() {
+		return ControlMain.getSettings();
+	}
+
+	private GuiSettingsTabMain getTab() {
+		return this.getSettingsTab().getSettingsTabMain();
+	}
 }
