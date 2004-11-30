11ce9f7b4df49682b7c03868d4d3a21a87477a81
crazyreini <crazyreini>
2004-11-30 12:07:50 +0000

    *** empty log message ***

--- jtg/service/SerHelper.java
+++ jtg/service/SerHelper.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.util.ArrayList;
 
 /*
-GuiTabProgramm.java  
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
