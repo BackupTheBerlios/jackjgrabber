7ad662c03a461e4f780e80c00b2b4efd2e7bf7bf
alexg <alexg>
2004-12-03 19:34:50 +0000

    *** empty log message ***

--- jtg/start.bat
+++ jtg/start.bat
@@ -1,2 +1,9 @@
-cd c:\Programme\jtg
-java -jar jackTheJGrabber.jar 
+echo off
+
+#HIER PFAD ZUM JRE BZW. ZUM JDK EINTRAGEN!!!
+set JAVA_HOME=C:\jdk1.5.0
+#HIER PFAD ZUM JTG-VERZEICHNIS EINTRAGEN!!!
+set JTJG_HOME=f:\jtg
+
+cd %JTJG_HOME%
+start %JAVA_HOME%\bin\javaw -jar jackTheJGrabber.jar
--- jtg/start.sh
+++ jtg/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-JAVA=/opt/jdk1.5.0/bin/java
+JAVA=/opt/jdk1.5.0
 JTGHOME=/home/ralph/jtg
+
 cd $JTGHOME
-$JAVA -jar jackTheJGrabber.jar
+$JAVA/bin/java -jar jackTheJGrabber.jar
