4eeab8bedd5ccc6ef75b8a2d43b968910426add3
crazyreini <crazyreini>
2004-12-15 12:52:27 +0000

    Record Modus gesetzt

--- jtg/streaming/RecordControl.java
+++ jtg/streaming/RecordControl.java
@@ -2,18 +2,14 @@ package streaming;
 /*
  * RecordControl.java by Geist Alexander
  * 
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option) any later version.
  * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 675 Mass
- * Ave, Cambridge, MA 02139, USA.
+ * You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation,
+ * Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *  
  */
 
@@ -35,7 +31,7 @@ import service.SerFormatter;
 import control.ControlMain;
 import control.ControlProgramTab;
 
-public class RecordControl extends Thread implements SerProcessStopListener{
+public class RecordControl extends Thread implements SerProcessStopListener {
 	Record record;
 	public boolean isRunning = true;
 	ControlProgramTab controlProgramTab;
@@ -71,6 +67,18 @@ public class RecordControl extends Thread implements SerProcessStopListener{
 
 		if (ControlMain.getSettingsRecord().isStoreEPG()) {
 			saveEPGInfos();
+		}
+
+		try {
+
+			if (ControlMain.getSettings().getRecordSettings().isStopPlaybackAtRecord()) {
+				ControlMain.getBoxAccess().setRecordModusWithPlayback();
+			} else {
+				ControlMain.getBoxAccess().setRecordModus();
+			}
+
+		} catch (IOException e) {
+			Logger.getLogger("RecordControl").error(e.getMessage());
 		}
 		record.start();
 
@@ -132,9 +140,9 @@ public class RecordControl extends Thread implements SerProcessStopListener{
 			}
 		}
 	}
-	
+
 	public void processStopped(int exitCode) {
-	    this.checkForShutdown();
+		this.checkForShutdown();
 	}
 
 	private void waitForStop() {
@@ -154,34 +162,39 @@ public class RecordControl extends Thread implements SerProcessStopListener{
 	public void stopRecord() {
 		record.stop();
 		if (ControlMain.getSettingsRecord().isStartPX() && record.getFiles() != null && record.getFiles().size() > 0) {
-		    Logger.getLogger("RecordControl").info(ControlMain.getProperty("msg_startPX"));
-		    this.startProjectX();
+			Logger.getLogger("RecordControl").info(ControlMain.getProperty("msg_startPX"));
+			this.startProjectX();
 		} else {
-		    this.checkForShutdown();
+			this.checkForShutdown();
 		}
 		isRunning = false;
+		try {
+			ControlMain.getBoxAccess().stopRecordModus();
+		} catch (IOException e) {
+			Logger.getLogger("RecordControl").error(e.getMessage());
+		}
 	}
-	
+
 	private void checkForShutdown() {
-	    if (recordArgs.isQuickRecord() && controlProgramTab.isShutdownAfterRecord()) {
-	        ControlMain.shutdownPC();
-	    }
-	    if (!recordArgs.isQuickRecord() && ControlMain.getSettingsRecord().isShutdownAfterRecord()) {
-	        ControlMain.shutdownPC();
-	    }
+		if (recordArgs.isQuickRecord() && controlProgramTab.isShutdownAfterRecord()) {
+			ControlMain.shutdownPC();
+		}
+		if (!recordArgs.isQuickRecord() && ControlMain.getSettingsRecord().isShutdownAfterRecord()) {
+			ControlMain.shutdownPC();
+		}
 	}
 
 	public void startProjectX() {
-	    ArrayList files = record.getFiles();
-	    String[] param = new String[3+files.size()];
+		ArrayList files = record.getFiles();
+		String[] param = new String[3 + files.size()];
 		String separator = System.getProperty("file.separator");
 
-		param[0]=System.getProperty("java.home") + separator + "bin" + separator + "java";
-		param[1]="-jar";
-		param[2]=ControlMain.getSettingsPath().getProjectXPath();
-		
+		param[0] = System.getProperty("java.home") + separator + "bin" + separator + "java";
+		param[1] = "-jar";
+		param[2] = ControlMain.getSettingsPath().getProjectXPath();
+
 		for (int i = 0; i < files.size(); i++) {
-		    param[i+3]=(String) files.get(i);
+			param[i + 3] = (String) files.get(i);
 		}
 
 		BOExternalProcess proc = SerExternalProcessHandler.startProcess(this, "ProjectX", param, true);
@@ -189,36 +202,29 @@ public class RecordControl extends Thread implements SerProcessStopListener{
 
 	public String getFileName() {
 		if (this.fileName == null) {
-			
-/*			SimpleDateFormat f = new SimpleDateFormat("dd-MM-yy_HH-mm");
-			String date = f.format(new Date());
 
-			BORecordArgs args = this.recordArgs;
-			if (args.getEpgTitle() != null) {
-				Object[] obj = {date, args.getSenderName(), args.getEpgTitle()};
-				MessageFormat form = new MessageFormat("{0}_{1}_{2}");
-				fileName = form.format(obj);
-			} else {
-				fileName = date + "_" + args.getSenderName();
-			}
-			*/
-			
+			/*
+			 * SimpleDateFormat f = new SimpleDateFormat("dd-MM-yy_HH-mm"); String date = f.format(new Date());
+			 * 
+			 * BORecordArgs args = this.recordArgs; if (args.getEpgTitle() != null) { Object[] obj = {date, args.getSenderName(),
+			 * args.getEpgTitle()}; MessageFormat form = new MessageFormat("{0}_{1}_{2}"); fileName = form.format(obj); } else { fileName =
+			 * date + "_" + args.getSenderName(); }
+			 */
+
 			String pattern = ControlMain.getSettingsRecord().getFilePattern();
-			
-			if (pattern == null || pattern.length() == 0)
-			{
+
+			if (pattern == null || pattern.length() == 0) {
 				pattern = ControlMain.getSettingsRecord().getDirPattern();
 			}
-			
-			fileName = SerHelper.createFileName(recordArgs,pattern);
-			
+
+			fileName = SerHelper.createFileName(recordArgs, pattern);
+
 			// create directory
-			String dirName = SerHelper.createFileName(recordArgs,ControlMain.getSettingsRecord().getDirPattern());
-			
-			
-			directory = new File(ControlMain.getSettingsPath().getSavePath(), SerFormatter.removeInvalidCharacters(dirName.replace(' ',
-			'_')));
-			directory.mkdir();			
+			String dirName = SerHelper.createFileName(recordArgs, ControlMain.getSettingsRecord().getDirPattern());
+
+			directory = new File(ControlMain.getSettingsPath().getSavePath(), SerFormatter.removeInvalidCharacters(dirName
+					.replace(' ', '_')));
+			directory.mkdir();
 		}
 		return SerFormatter.removeInvalidCharacters(fileName.replace(' ', '_'));
 	}
