b281c13b9d52ca6cc88ee7224ea4a8865c0a7ddc
ralix <ralix>
2005-01-18 10:54:05 +0000

    Test für SucheListe

--- jtg/presentation/movieguide/GuiTabMovieGuide.java
+++ jtg/presentation/movieguide/GuiTabMovieGuide.java
@@ -333,10 +333,12 @@ public class GuiTabMovieGuide extends JPanel {
 			ImageIcon icon = new ImageIcon(ClassLoader.getSystemResource("ico/Refresh16.gif"));
 			jButtonNeuEinlesen.setIcon(icon);		
 			jButtonNeuEinlesen.setText(ControlMain.getProperty("button_new_read"));
+			//jButtonNeuEinlesen.setText("RecordListe");
 			jButtonNeuEinlesen.setActionCommand("neuEinlesen");
 			jButtonNeuEinlesen.setToolTipText(ControlMain.getProperty("button_new_read_txt"));
+			//jButtonNeuEinlesen.setToolTipText("Liste der nicht zu verpassenden Filme");
 			jButtonNeuEinlesen.addActionListener(this.getControl());		
-			jButtonNeuEinlesen.setEnabled(false);
+			//jButtonNeuEinlesen.setEnabled(false);
 		}
 		return jButtonNeuEinlesen;
 	}
