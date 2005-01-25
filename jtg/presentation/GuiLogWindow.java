cf80675892d3b6e58fa259893ddb11f0c9e4d74e
crazyreini <crazyreini>
2005-01-25 15:28:27 +0000

    IllegalArgumentException bei Log löschen und ausblenden behoben

--- jtg/presentation/GuiLogWindow.java
+++ jtg/presentation/GuiLogWindow.java
@@ -1,22 +1,17 @@
 package presentation;
 /*
-GuiLogWindow.java by Geist Alexander 
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2, or (at your option)
-any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  
-
-*/ 
+ * GuiLogWindow.java by Geist Alexander
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation,
+ * Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ */
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -42,38 +37,37 @@ import com.jgoodies.forms.layout.FormLayout;
 import control.ControlMain;
 
 public class GuiLogWindow extends JFrame {
-    
-    private JTextArea logArea;
-    private JScrollPane jScrollPaneLogArea;
-    private JPanel jPanelOutput;
-    private boolean shouldBeVisible;
-    
-    public GuiLogWindow() {
-        super("Log Jack the JGrabber");
-    	setContentPane(getJPanelOutput()); 
-        setSize(600 , 150);
-        
-        final ActionListener listener = new ActionListener() {
-            public final void actionPerformed(final ActionEvent e) {
-                switchLogVisiblity();
-            }
-        };
-        final KeyStroke keyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_L, InputEvent.CTRL_MASK, true);
-		getRootPane().registerKeyboardAction(listener, keyStroke, JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
-		
-		addWindowListener(new WindowAdapter()
-				{
-					public void windowClosing(WindowEvent e) {
-						JButton switchButton = ControlMain.getControl().getView().getTabProgramm().getJButtonSwitchLog();
-			            switchButton.setText(ControlMain.getProperty("button_on"));
-					    ControlMain.logWindow.setVisible(false);
-					    ControlMain.logWindow.setShouldBeVisible(false);
 
-					}
-				});
-    }
-    
-    /**
+	private JTextArea logArea;
+	private JScrollPane jScrollPaneLogArea;
+	private JPanel jPanelOutput;
+	private boolean shouldBeVisible;
+
+	public GuiLogWindow() {
+		super("Log Jack the JGrabber");
+		setContentPane(getJPanelOutput());
+		setSize(600, 150);
+
+		final ActionListener listener = new ActionListener() {
+			public final void actionPerformed(final ActionEvent e) {
+				switchLogVisiblity();
+			}
+		};
+		final KeyStroke keyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_L, InputEvent.CTRL_MASK, true);
+		getRootPane().registerKeyboardAction(listener, keyStroke, JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				JButton switchButton = ControlMain.getControl().getView().getTabProgramm().getJButtonSwitchLog();
+				switchButton.setText(ControlMain.getProperty("button_on"));
+				ControlMain.logWindow.setVisible(false);
+				ControlMain.logWindow.setShouldBeVisible(false);
+
+			}
+		});
+	}
+
+	/**
 	 * This method initializes jPanelOutput
 	 * 
 	 * @return javax.swing.JPanel
@@ -90,61 +84,64 @@ public class GuiLogWindow extends JFrame {
 		}
 		return jPanelOutput;
 	}
-    
-    /**
+
+	/**
 	 * This method initializes logArea
 	 * 
 	 * @return javax.swing.JTextPane
 	 */
 	public JTextArea getLogArea() {
 		if (logArea == null) {
-		    logArea = new JTextArea();
-		    logArea.setEditable(false);
-		    SerLogAppender.getTextAreas().add(logArea);
+			logArea = new JTextArea();
+			logArea.setEditable(false);
+			SerLogAppender.getTextAreas().add(logArea);
 		}
 		return logArea;
 	}
 
-    /**
-     * @return Returns the jScrollPaneLogArea.
-     */
-    public JScrollPane getJScrollPaneLogArea() {
-        if (jScrollPaneLogArea == null) {
-            jScrollPaneLogArea = new JScrollPane();
+	/**
+	 * @return Returns the jScrollPaneLogArea.
+	 */
+	public JScrollPane getJScrollPaneLogArea() {
+		if (jScrollPaneLogArea == null) {
+			jScrollPaneLogArea = new JScrollPane();
 			jScrollPaneLogArea.setViewportView(getLogArea());
 		}
-        return jScrollPaneLogArea;
-    }
-    /**
-     * @return Returns the shouldBeVisible.
-     */
-    public boolean isShouldBeVisible() {
-        return shouldBeVisible;
-    }
-    /**
-     * @param shouldBeVisible The shouldBeVisible to set.
-     */
-    public void setShouldBeVisible(boolean shouldBeVisible) {
-        this.shouldBeVisible = shouldBeVisible;
-    }
-    public void setVisible(boolean value) {
-        if (isShouldBeVisible()) {
-            super.setVisible(value);
-            this.getLogArea().setCaretPosition(getLogArea().getDocument().getLength()-1);
-        }
-    }
-    public static void switchLogVisiblity() {
-        JButton switchButton = ControlMain.getControl().getView().getTabProgramm().getJButtonSwitchLog();
-        if (ControlMain.logWindow.isVisible()) {
-            switchButton.setText(ControlMain.getProperty("button_on"));
-		    ControlMain.logWindow.setVisible(false);
-		    ControlMain.logWindow.setShouldBeVisible(false);
-		} else {
-		    switchButton.setText(ControlMain.getProperty("button_off"));
-		    ControlMain.logWindow.setShouldBeVisible(true);
-		    ControlMain.logWindow.setVisible(true);
+		return jScrollPaneLogArea;
+	}
+	/**
+	 * @return Returns the shouldBeVisible.
+	 */
+	public boolean isShouldBeVisible() {
+		return shouldBeVisible;
+	}
+	/**
+	 * @param shouldBeVisible
+	 *            The shouldBeVisible to set.
+	 */
+	public void setShouldBeVisible(boolean shouldBeVisible) {
+		this.shouldBeVisible = shouldBeVisible;
+	}
+	public void setVisible(boolean value) {
+		if (isShouldBeVisible()) {
+			super.setVisible(value);
+			int pos = getLogArea().getDocument().getLength() - 1;
+			if (pos > -1) {
+				this.getLogArea().setCaretPosition(pos);
+			}
 		}
-    }
+	}
+	public static void switchLogVisiblity() {
+		JButton switchButton = ControlMain.getControl().getView().getTabProgramm().getJButtonSwitchLog();
+		if (ControlMain.logWindow.isVisible()) {
+			switchButton.setText(ControlMain.getProperty("button_on"));
+			ControlMain.logWindow.setVisible(false);
+			ControlMain.logWindow.setShouldBeVisible(false);
+		} else {
+			switchButton.setText(ControlMain.getProperty("button_off"));
+			ControlMain.logWindow.setShouldBeVisible(true);
+			ControlMain.logWindow.setVisible(true);
+		}
+	}
 }
-        
 
