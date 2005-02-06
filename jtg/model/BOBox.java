4b124e3a830718913ea57fd06a2faaa2d2002211
alexg <alexg>
2005-02-06 20:16:39 +0000

    changed konstruktor

--- jtg/model/BOBox.java
+++ jtg/model/BOBox.java
@@ -23,12 +23,6 @@ public class BOBox {
 	public boolean standard = false;
 	private boolean selected;
 
-	public BOBox() {
-	}
-
-	public BOBox(String ip) {
-		this.setDboxIp(ip);
-	}
 	/**
 	 * @return Returns the dboxIp.
 	 */
